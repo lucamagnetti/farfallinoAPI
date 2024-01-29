7f5f7fe02a5df1030fe666471c37ca311dba0369
Luca <l.magnetti@naxa.ws>
2024-01-29 16:11:16 +0100

    add info

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'FarfallinoAPI ti da il benvenuto, inizia subito facendo una chiamata GET a /farfallino?text=[STRING]';
   }
 }
